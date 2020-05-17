fc4ae359f5aba28d42523489c98492be774d3880
Deramecourt Mathurin <mathurin9@hotmail.fr>
2020-05-17 20:03:11 +0200

    Parsing bool and number as literal

--- SimpleFastTurtle/lexer.c
+++ SimpleFastTurtle/lexer.c
@@ -16,6 +16,8 @@
 static void lexer_open_recur(char *file_name);
 static void lexer_write_token(FILE *f, int line, char name[], int id, char token[]);
 static void lexer_analyze(FILE *f);
+static void lexer_test(FILE *output, int current_line, int token_index, char current_token[]);
+static int lexer_is_literal(char token[]);
 static int lexer_is_keyword(char token[]);
 static int lexer_is_operator(char token);
 static int lexer_is_separator(char token);
@@ -66,29 +68,14 @@ static void lexer_analyze(FILE *f) //temp
     {
         if (c == '\n' || c == '\t' || c == ' ')
         {
-            if (token_index > 0)
-            {
-                current_token[token_index] = '\0';
-                if (current_token[0] > 97 && current_token[0] < 120)
-                {
-                    int result = lexer_is_keyword(current_token);
-                    if (result)
-                        lexer_write_token(new_f, current_line, "KEY", result, current_token);
-                    else
-                        lexer_write_token(new_f, current_line, "ID", 0, current_token);
-                }
-                else
-                {
-                    lexer_write_token(new_f, current_line, "ID", 0, current_token);
-                }
-                token_index = 0;
-            }
+            lexer_test(new_f, current_line, token_index, current_token);
+            token_index = 0;
+
             if (c == '\n')
                 current_line++;
         }
         else
         {
-            //can be optimized
             int is_operator = lexer_is_operator(c);
             int is_separator = 0;
             if (!is_operator)
@@ -96,24 +83,9 @@ static void lexer_analyze(FILE *f) //temp
 
             if (is_operator || is_separator)
             {
-                if (token_index > 0)
-                {
-                    current_token[token_index] = '\0';
-                    if (current_token[0] > 97 && current_token[0] < 120)
-                    {
-                        int result = lexer_is_keyword(current_token);
-                        if (result)
-                            lexer_write_token(new_f, current_line, "KEY", result, current_token);
-                        else
-                            lexer_write_token(new_f, current_line, "ID", 0, current_token);
-                    }
-                    else
-                    {
-                        lexer_write_token(new_f, current_line, "ID", 0, current_token);
-                    }
-                }
-
+                lexer_test(new_f, current_line, token_index, current_token);
                 token_index = 0;
+
                 current_token[0] = c;
                 current_token[1] = '\0';
 
@@ -135,58 +107,96 @@ static void lexer_analyze(FILE *f) //temp
     printf("Number of line : %d\n", current_line+1);
 }
 
+static void lexer_test(FILE *output, int current_line, int token_index, char current_token[])
+{
+    if (token_index > 0)
+    {
+        current_token[token_index] = '\0';
+
+        int result = lexer_is_keyword(current_token);
+        if (result)
+        {
+            lexer_write_token(output, current_line, "KEY", result, current_token);
+            return;
+        }
+
+        result = lexer_is_literal(current_token);
+        if (result)
+        {
+            lexer_write_token(output, current_line, "LI", result, current_token);
+            return;
+        }
+
+        lexer_write_token(output, current_line, "ID", 0, current_token);
+    }
+}
+
+/*Note that string and char are not taken into account here since they require special state*/
+static int lexer_is_literal(char token[])
+{
+    int first_char = token[0];
+    if (first_char >= 48 && first_char <= 57)
+        return TOK_LI_NUMBER;
+    if ((first_char == 't' || first_char == 'f') && (strcmp("true", token) || strcmp("false", token)))
+        return TOK_LI_BOOL;
+    return 0;
+}
+
 static int lexer_is_keyword(char token[])
 {
-    switch(token[0])
+    if (token[0] > 97 && token[0] < 120)
     {
-        case 'f':
-            if (!strcmp("for", token))
-                return TOK_KEY_FOR;
-            else if (!strcmp("float", token))
-                return TOK_KEY_FLOAT;
-            break;
-        case 'i':
-            if (!strcmp("if", token))
-                return TOK_KEY_IF;
-            else if (!strcmp("int", token))
-                return TOK_KEY_INT;
-            break;
-        case 'w':
-            if (!strcmp("while", token))
-                return TOK_KEY_WHILE;
-            break;
-        case 'e':
-            if (!strcmp("else", token))
-                return TOK_KEY_ELSE;
-            break;
-        case 'b':
-            if (!strcmp("bool", token))
-                return TOK_KEY_BOOL;
-            else if (!strcmp("break", token))
-                return TOK_KEY_BREAK;
-            break;
-        case 'c':
-            if (!strcmp("char", token))
-                return TOK_KEY_CHAR;
-            break;
-        case 'r':
-            if (!strcmp("return", token))
-                return TOK_KEY_RETURN;
-            break;
-        case 's':
-            if (!strcmp("str", token))
-                return TOK_KEY_STR;
-            else if (!strcmp("switch", token))
-                return TOK_KEY_SWITCH;
-            else if (!strcmp("static", token))
-                return TOK_KEY_STATIC;
-            break;
-        case 'd':
-            if (!strcmp("default", token))
-                return TOK_KEY_DEFAULT;
-            break;
-        default:
-            return 0;
+        switch(token[0])
+        {
+            case 'f':
+                if (!strcmp("for", token))
+                    return TOK_KEY_FOR;
+                if (!strcmp("float", token))
+                    return TOK_KEY_FLOAT;
+                break;
+            case 'i':
+                if (!strcmp("if", token))
+                    return TOK_KEY_IF;
+                if (!strcmp("int", token))
+                    return TOK_KEY_INT;
+                break;
+            case 'w':
+                if (!strcmp("while", token))
+                    return TOK_KEY_WHILE;
+                break;
+            case 'e':
+                if (!strcmp("else", token))
+                    return TOK_KEY_ELSE;
+                break;
+            case 'b':
+                if (!strcmp("bool", token))
+                    return TOK_KEY_BOOL;
+                if (!strcmp("break", token))
+                    return TOK_KEY_BREAK;
+                break;
+            case 'c':
+                if (!strcmp("char", token))
+                    return TOK_KEY_CHAR;
+                break;
+            case 'r':
+                if (!strcmp("return", token))
+                    return TOK_KEY_RETURN;
+                break;
+            case 's':
+                if (!strcmp("str", token))
+                    return TOK_KEY_STR;
+                if (!strcmp("switch", token))
+                    return TOK_KEY_SWITCH;
+                if (!strcmp("static", token))
+                    return TOK_KEY_STATIC;
+                break;
+            case 'd':
+                if (!strcmp("default", token))
+                    return TOK_KEY_DEFAULT;
+                break;
+            default:
+                return 0;
+        }
     }
     return 0;
 }
